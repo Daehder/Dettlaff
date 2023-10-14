0e185095b9a60596152e1989231cb811f1e7c476
Daehder <erikm95@me.com>
2023-10-14 23:27:42 +0000

    fixup! First crack at moving flywheels into their own task

--- src/flywheelTask.cpp
+++ src/flywheelTask.cpp
@@ -1,16 +1,34 @@
-#include "freertos/queue.h"
+/* clang-format off */
+/* The FreeRTOS.h include needs to happen before the others can be included*/
 #include <freertos/FreeRTOS.h>
+/* clang-format off */
+#include "freertos/queue.h"
 #include <freertos/task.h>
 
-const uint32_t cFlywheelTaskNotifyTimerBitFlag = 0x01;
-const uint32_t cFlywheelTaskNotifySpindownBitFlag = 0x02;
-const uint32_t cFlywheelTaskNotifyIdleBitFlag = 0x04;
-const uint32_t cFlywheelTaskNotifyRevBitFlag = 0x08;
-const uint32_t cFlywheelTaskNotifyRpmNextBitFlag = 0x10;
-const uint32_t cFlywheelTaskNotifyRpmPreviousBitFlag = 0x20;
-const uint32_t cFlywheelTaskNotifyRpmSetpoint1BitFlag = 0x40;
-const uint32_t cFlywheelTaskNotifyRpmSetpoint2BitFlag = 0x80;
-const uint32_t cFlywheelTaskNotifyRpmSetpoint3BitFlag = 0x100;
+/* These are the only bits needed to spin up the flywheels
+ * The pusher should set the Fire request bit when it wants the flywheels up to speed, then wait for its Up to Speed bit to be set */
+const uint32_t cFlywheelTaskNotifyFireRequestBitFlag = 0x01;
+/* Once the pusher has successfully fired all darts, it should clear the Fire Request bit to let the flywheels idle, then spindown (or spin up again) */
+const uint32_t cFlywheelTaskNotifyClearFireRequestBitFlag = 0x02;
+/* This is an internal bit that will kick the PID loop; calling this from any other task will cause undefined behvior in the PID loop*/
+const uint32_t cFlywheelTaskNotifyTimerBitFlag = 0x04;
+/* This task will normally govern spindown and this should not need to be used
+ * This bit is for error cases where something else has happened, like a pusher jam*/
+const uint32_t cFlywheelTaskNotifySpindownBitFlag = 0x08;
+/* This task will normally govern idle state after darts are fired without needing this bit to be set
+ * This bit is for competitive modes or short idle buttons initiated from the UX*/
+const uint32_t cFlywheelTaskNotifyIdleBitFlag = 0x10;
+const uint32_t cFlywheelTaskNotifyClearIdleBitFlag = 0x20;
+/* This task will normally govern rev states by the fire request bit without needing this bit to be set
+ * This bit is for seperate rev triggers (or takeup) */
+const uint32_t cFlywheelTaskNotifyRevBitFlag = 0x40;
+const uint32_t cFlywheelTaskNotifyClearRevBitFlag = 0x80;
+/* These are the UX bits. Setting them will do the named action once; no need to clear them, as this task will do that after every notification*/
+const uint32_t cFlywheelTaskNotifyRpmNextBitFlag = 0x100;
+const uint32_t cFlywheelTaskNotifyRpmPreviousBitFlag = 0x200;
+const uint32_t cFlywheelTaskNotifyRpmSetpoint1BitFlag = 0x400;
+const uint32_t cFlywheelTaskNotifyRpmSetpoint2BitFlag = 0x800;
+const uint32_t cFlywheelTaskNotifyRpmSetpoint3BitFlag = 0x1000;
 
 const uint32_t cWaitForZerothNotification = 0;
 const uint32_t cDontClearBits = 0;
@@ -32,14 +50,33 @@ void flywheelTask(void* arg)
 
         // TODO(Daehder) how does the pusher inform us that it's done pushing darts and to idle/spindown?
 
+        /* Spindown has the highest priority in case something needs to abort flywheel motion*/
+        if ((flywheelTaskNotifyBits & cFlywheelTaskNotifySpindownBitFlag) != 0) {
+            // spindown
+        }
+        
+        // TODO(Daehder) If we get both rev and clear, should we rev? clear?
+        if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyClearFireRequestBitFlag) != 0) {
+            // clear rev state
+        }
+        else if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyFireRequestBitFlag) != 0) {
+            // rev
+            // Cancel spindown timer
+        } 
+        
         if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyRevBitFlag) != 0) {
             // rev
             // Cancel spindown timer
-        } else if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyIdleBitFlag) != 0) {
+        } else if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyClearRevBitFlag) != 0) {
+            // clear rev state
+        } 
+        
+        if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyIdleBitFlag) != 0) {
             // Idle
             // Kick Spindown timer
-        } else if ((flywheelTaskNotifyBits & cFlywheelTaskNotifySpindownBitFlag) != 0) {
-            // spindown
+        } else if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyClearIdleBitFlag) != 0) {
+            // Idle
+            // Kick Spindown timer
         }
 
         if ((flywheelTaskNotifyBits & cFlywheelTaskNotifyRpmNextBitFlag) != 0) {
